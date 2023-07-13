7aa734be531f823200e86048e03bcd14bc878ba0
RohithSirigadde <111242730+RohithSirigadde@users.noreply.github.com>
2023-07-13 23:54:08 +0530

    Update 2.2-DFS.py

--- search /2.2-DFS.py
+++ search /2.2-DFS.py
@@ -1,15 +1,21 @@
-class Stack(Generic[T]):
-   def __init__(self) -> None:
-      self._container: List[T] = []
-   @property
-   def empty(self) -> bool:
-       return not self._container # not is true for empty container
-     
-   def push(self, item: T) -> None:
-      self._container.append(item)
-     
-   def pop(self) -> T:
-      return self._container.pop() # LIFO
-     
-   def __repr__(self) -> str:
-      return repr(self._container)
+def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T],
+      List[T]]) -> Optional[Node[T]]:
+       # frontier is where we've yet to go
+    frontier: Stack[Node[T]] = Stack()
+    frontier.push(Node(initial, None))
+ # explored is where we've been
+    explored: Set[T] = {initial}
+ # keep going while there is more to explore
+ while not frontier.empty:
+    current_node: Node[T] = frontier.pop()
+    current_state: T = current_node.state
+ # if we found the goal, we're done
+    if goal_test(current_state):
+       return current_node
+ # check where we can go next and haven't explored
+    for child in successors(current_state):
+       if child in explored: # skip children we already explored
+          continue
+       explored.add(child)
+       frontier.push(Node(child, current_node))
+ return None # went through everything and never found goal
