d65be8fa36b2ceaa7de1df6fbb88d67f4f05e0f2
RohithSirigadde <111242730+RohithSirigadde@users.noreply.github.com>
2023-07-13 23:50:56 +0530

    Create 2.2-DFS.py

---
+++ search /2.2-DFS.py
@@ -0,0 +1,15 @@
+class Stack(Generic[T]):
+   def __init__(self) -> None:
+      self._container: List[T] = []
+   @property
+   def empty(self) -> bool:
+       return not self._container # not is true for empty container
+     
+   def push(self, item: T) -> None:
+      self._container.append(item)
+     
+   def pop(self) -> T:
+      return self._container.pop() # LIFO
+     
+   def __repr__(self) -> str:
+      return repr(self._container)
